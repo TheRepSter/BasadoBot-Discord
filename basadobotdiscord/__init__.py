88221d37534b3752b181d9c1dea3e97dc39e652d
TheRepSter <NULL>
2021-08-18 18:47:17 +0200

    BasadoBot v1.0.0 It lives!

--- basadobotdiscord/__init__.py
+++ basadobotdiscord/__init__.py
@@ -7,61 +7,94 @@
 
 import discord
 from discord.ext import commands
+from basadobotdiscord.models import User, session
+from basadobotdiscord.cunado import generador_frase
+from time import time
+intents = discord.Intents.default()
+intents.members = True
 
-messages = {
-            1:"Casa de cartas",
-            5:"Árbol joven",
-            10:"Silla de oficina",
-            20:"Canasta de baloncesto (llena de arena)",
-            35:"Luchador de sumo",
-            50:"Fundación de hormigón",
-            75:"Sequoia gigante",
-            100:"Empire State Building",
-            200:"Gran pirámide de Giza",
-            350:"Edificio de ensamblaje de vehículos de la ESA",
-            500:"Fábrica de Boeing Everett",
-            750:"Monte Fuji", 
-            1000:"Denali",
-            2000:"Annapurna"}
+discord.MemberCacheFlags.from_intents(intents=intents)
 
-messageInfo = "\n\n".join([
-                    "¡Hola! ¡Soy un bot llamado BasadoBot!",
-                    "He sido creado por u/TheRepSter, por simple diversión.",
-                    "Cuento \"basados\", es decir, cuando estás de acuerdo con una persona.",
-                    "También llevo la cuenta de las píldoras que tiene cada usuario.",
-                    "Los comandos son los siguientes:",
-                    "- /info: muestra este mensaje.",
-                    "- /usuariosmasbasados (o /usuariosmásbasados): muestra el top 10 de basados.",
-                    "- /cantidaddebasado \{username\}: muestra los basados según el username",
-                    "- /tirarpildora \{píldora\}: tira la píldora que mencione el usuario que pone el comando",
-                    ""
-                    "Soy de código abierto, es decir, ¡puedes ver mi código e incluso aportar!",
-                    "[Haz click aquí para ver el código.](https://github.com/TheRepSter/BasadoBot-Discord)",
-                    "¿Tienes alguna duda? ¡Háblame por MD a mi o a mi creador!"
-                ]) 
+messageInfo=discord.Embed(title="¡Hola! ¡Soy un bot llamado BasadoBot!", description="He sido creado por @RepSter#6969, por simple diversión.\nCuento \"basados\", es decir, cuando estás de acuerdo con una persona.\nTambién llevo la cuenta de las píldoras que tiene cada usuario.\nLos comandos son los siguientes:", color=0xff0000)
+messageInfo.set_author(name="BasadoBot", url="https://github.com/TheRepSter/BasadoBot-Discord")
+messageInfo.add_field(name="b!info o b!help", value="Muestra este mensaje.", inline=False)
+messageInfo.add_field(name="b!masbasados", value="Muestra el top 10 de basados.", inline=False)
+messageInfo.add_field(name="b!cantidaddebasado", value="Muestra los basados según el username.", inline=False)
+messageInfo.add_field(name="b!frasecunada", value="Envia una frase de cuñado aleatoria.", inline=False)
+messageInfo.add_field(name="b!basado", value="Para basar a una persona", inline=False)
+messageInfo.add_field(name="Soy de código abierto, es decir, ¡puedes ver mi código e incluso aportar!", value=" ¡Dale click arriba del todo y entrarás en mi github!", inline=False)
+messageInfo.set_footer(text="¿Tienes alguna duda? ¡Habla por MD a mi creador!")
 
-bot = commands.Bot(command_prefix="b")
+bot = commands.Bot(command_prefix="b!", help_command=None, intents=intents)
 
-@commands.command(name="info")
+@bot.command(name="info", aliases=["help"])
 async def info(ctx):
-    await ctx.send(messageInfo)
+    await ctx.send(embed=messageInfo)
 
-@commands.command()
-async def asado(ctx, member: discord.Member, pill:str):
-    pass
+@bot.command(name="basado")
+async def basado(ctx, member: discord.Member = None):
+    if ctx.message.author.id == member.id:
+        await ctx.send("Eres tonto.")
+        return
+    
+    if member == None:
+        await ctx.send("Tienes que poner algun miembro.")
+        return
+        
+    personaQueDaBasado = session.query(User).filter(User.userid == str(ctx.message.author.id), User.serverid == str(ctx.guild.id)).first()
+    if not personaQueDaBasado:
+        personaQueDaBasado = User(serverid = str(ctx.guild.id), userid = str(ctx.message.author.id), ultimobasado = 0, basados = 0)
 
-@commands.command()
+    if time() - personaQueDaBasado.ultimobasado < 60:
+        await ctx.send("¡No puedes dar basados tan rápido!")
+        personaQueDaBasado.ultimobasado = time()
+        session.add(personaQueDaBasado)
+        session.commit()
+        return
+    
+    personaQueDaBasado.ultimobasado = time()
+
+    personaBasada = session.query(User).filter(User.userid == str(member.id), User.serverid == str(ctx.guild.id)).first()
+    if not personaBasada:
+        personaBasada = User(serverid = str(ctx.guild.id), userid = str(member.id), ultimobasado = 0, basados = 0)
+
+    personaBasada.basados += 1
+    
+    session.add(personaQueDaBasado)
+    session.add(personaBasada)
+    session.commit()
+    await ctx.send(f"Ahora {member.display_name} tiene {personaBasada.basados} basados.")
+
+@bot.command(name="masbasados", aliases=["másbasados", "topbasados"])
 async def masbasados(ctx):
-    pass
+    topBasado = discord.Embed(title="El Top 10 de los usuarios más basados en el servidor es actualmente:")
+    usuarios = session.query(User).filter(User.serverid == str(ctx.guild.id)).order_by(User.basados.desc()).limit(10).all()
+    for numb, i in enumerate(usuarios):
+        topBasado.add_field(name=str(numb+1), value=bot.get_user(int(i.userid)).display_name, inline=True)
+    
+    await ctx.send(embed=topBasado)
 
-@commands.command()
-async def cantidaddebasado(ctx):
-    pass
+@bot.command()
+async def cantidaddebasado(ctx, member: discord.Member = None):
+    if member == None:
+        member = ctx.message.author
+    usuarioServer = session.query(User).filter(User.userid == str(member.id), User.serverid == str(ctx.guild.id)).first()
+    if usuarioServer:
+        usuarioAll = session.query(User).filter(User.userid == str(member.id)).all()
+        basadosTotales : int = 0
+        for i in usuarioAll:
+            basadosTotales += i.basados
 
-@commands.command()
-async def tirarpildora(ctx, pill:str):
-    pass
+        message = discord.Embed(title=member.display_name)
+        message.add_field(name="Basados en este servidor:", value=usuarioServer.basados, inline=False)
+        message.add_field(name="Basados totales:", value=basadosTotales, inline=False)
+        await ctx.send(embed=message)
+    else:
+        await ctx.send("Este usuario no existe")
 
+@bot.command(name="frasecunada",aliases=["frasecunado", "frasecuñado", "frasecuñada","frasedecunada","frasedecunado", "frasedecuñado", "frasedecuñada"])
+async def frasecunada(ctx):
+    await ctx.send(generador_frase(ctx.message.author.display_name))
 
 def run(token):
     bot.run(token)
--- basadobotdiscord/models.py
+++ basadobotdiscord/models.py
@@ -1,11 +1,21 @@
-#User:
-# - id:int
-# - serverid:str
-# - userid:str
-# - baseds:int+
-# - pills:list[Pill]
+from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 
-#Pill:
-# - id:int
-# - name:str
-# - user:User
+engine = create_engine('sqlite:///data.db')
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    serverid = Column(String(18), nullable=False)
+    userid = Column(String(18), nullable=False)
+    basados = Column(Integer, default=0)
+    ultimobasado = Column(Float, default=0)
+
+    def __repr__(self) -> str:
+        return f"User(serverid={self.serverid}, userid={self.userid}, basados={self.basados}, ultimobasado={self.ultimobasado})"
+
+sessionClass = sessionmaker(engine)
+session = sessionClass()
